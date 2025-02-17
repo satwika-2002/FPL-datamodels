6bb2531ba32f3250f320303b4e9075971a3ce480
satwika <sathvikachowdary119@gmail.com>
2025-02-17 15:07:53 +0530

    left join

--- ques1.sql
+++ ques1.sql
@@ -1,1 +1,1 @@
-select * from products;
+select p.product_name,p.category,i.quantity from products p left join inventory i on p.product_id = i.inventory_id and i.warehouse_id = 1 order by i.quantity desc;
