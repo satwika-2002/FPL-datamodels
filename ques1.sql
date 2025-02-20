5ad7df4274eda6e45289cd1d5c385a9a9f932baf
satwika <sathvikachowdary119@gmail.com>
2025-02-20 16:59:33 +0530

    semi join

--- ques1.sql
+++ ques1.sql
@@ -1,1 +1,5 @@
 select p.product_name,p.category,i.quantity from products p left join inventory i on p.product_id = i.inventory_id and i.warehouse_id = 1 order by i.quantity desc;
+# the question collects the data from products,inventory and warehouse TABLES
+#inventory table is holding foreign keys REFERENCES
+# we will join the tables by checking/equating the id's from these tables
+# what if invetory table is holding only one table id?
