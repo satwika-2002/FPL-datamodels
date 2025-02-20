fc7ea507fe5edfdbea16454a975bd27998b49f69
satwika <sathvikachowdary119@gmail.com>
2025-02-20 20:02:36 +0530

    sql

--- ques5.sql
+++ ques5.sql
@@ -1,6 +1,6 @@
 SELECT 
     w.location,
-    COUNT(p.product_id) as product_count,
+    COUNT(p.category) as product_count,
     AVG(i.quantity) as avg_quantity,
     sum(i.quantity * p.base_price) as total_value,
     max(i.quantity) as min_stock,
@@ -16,3 +16,6 @@ GROUP BY
     w.location
 having
 	sum(i.quantity * p.base_price) > 50000;
+#didn't understand product count(based on id or name)
+#performed different aggregate functions on columns of different tables
+#used joins coz there is relation b/w tables and we calculated one value based on relation
