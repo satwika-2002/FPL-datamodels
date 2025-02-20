057d6a0978544feef2c4cd88f36115d1bf607e4a
satwika <sathvikachowdary119@gmail.com>
2025-02-20 20:15:31 +0530

    function

---
+++ ques9.sql
@@ -0,0 +1,18 @@
+CREATE FUNCTION calculate_inventory_metrics(warehouse_id INT)
+RETURNS TABLE (
+    total_products INT,
+    total_value INT,
+    avg_quantity DECIMAL(10,2),
+    most_stocked_category VARCHAR(255)
+) AS 
+BEGIN
+    RETURN QUERY
+    SELECT 
+        COUNT(p.product_id) as total_product,sum(i.quantity * p.base_price) as total_value,AVG(i.quantity) as avg_quantity,(select p.category FROM products p JOIN inventory i ON p.product_id = i.product_id join warehouses w i.warehouse_id = w.warehouse_id = 1 GROUP BY p.category ORDER BY SUM(i.quantity) DESC) AS most_stocked_category
+    FROM 
+        products p
+    JOIN 
+        inventory i ON p.product_id = i.product_id
+    join warehouses w on w.warehouse_id = i.warehouse_id = 1
+END;
+#
