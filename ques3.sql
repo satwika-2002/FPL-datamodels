1a5426e86936f6583a9ff2a8b41bda23e3f329be
satwika <sathvikachowdary119@gmail.com>
2025-02-21 11:35:06 +0530

    corrected exists statement

--- ques3.sql
+++ ques3.sql
@@ -1,5 +1,6 @@
-select s.supplier_name,s.country,s.reliability_score from suppliers s where 
-exists ( select 1 from inventory i where i.quantity > 100 );
+select s.supplier_name,s.country,s.reliability_score from suppliers s where
+exists(select 1 from products p join inventory i on p.product_id = i.product_id where i.quantity < 100)
+order by s.reliability_score desc;
 #semi join just matches the value 
 #it will display only one tables values(first tables)
 #we will just check the condition if it matches then it will return firstvtable values
