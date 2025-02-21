80f1760e05f89f2c1eebd3036717b75a0fc9e3b0
satwika <sathvikachowdary119@gmail.com>
2025-02-21 18:30:33 +0530

    Update ques2.sql

--- ques2.sql
+++ ques2.sql
@@ -12,9 +12,7 @@ Chicago, IL | 275           | 21997.25
 select w.location,i.quantity,sum(i.quantity * p.base_price) as total_value from warehouses w inner join inventory i on w.warehouse_id = i.warehouse_id inner join products p on p.product_id = i.product_id
 where p.category = 'Electronics' and sum(i.quantity * p.base_price) > 20000
 order by total_value desc;
-#create a new column that contain value
-#listed only warehouses that contain electronics whose value is greater than 20000
-#order by total value in descending order 
+
 --comments:
 --1. The query joins the warehouses, inventory, and products tables on the respective columns.
 --2. It filters the rows where the product category is 'Electronics' and the total value of the inventory is greater than $20,000.
