af17e7216f2098822c4e4fe079eefc64eccbb465
satwika <sathvikachowdary119@gmail.com>
2025-02-21 12:36:32 +0530

    ques6-merge

--- ques6.sql
+++ ques6.sql
@@ -6,8 +6,8 @@ CREATE TEMP TABLE inventory_updates (
 --insert statement that check weather data is already present or not
 insert into inventory_updates(product_id,warehouse_id,new_quantity)
 select 1,1,200
-where not exists(select 1 from inventory_updates where product_id and warehouse_id = 1);
+where not exists(select 1 from inventory_updates where product_id = 1 and warehouse_id = 1);
 --update statement 
 update inventory_updates
 set new_quantity = 300
-where exists(select 1 from inventory_updates where product_id and warehouse_id = 1);
+where exists(select 1 from inventory_updates where product_id = 1 and warehouse_id = 1);
