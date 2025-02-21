967827a907872a617b67c13016e557454a3c2459
satwika <sathvikachowdary119@gmail.com>
2025-02-21 11:22:34 +0530

    updated join statement

--- ques1.sql
+++ ques1.sql
@@ -1,4 +1,4 @@
-select p.product_name,p.category,i.quantity from products p left join inventory i on p.product_id = i.inventory_id and i.warehouse_id = 1 order by i.quantity desc;
+select p.product_name,p.category,i.quantity,w.location from products p left join inventory i on p.product_id = i.product_id and i.warehouse_id = 1 left join warehouses w on w.warehouse_id = 1 order by i.quantity desc;
 # the question collects the data from products,inventory and warehouse TABLES
 #inventory table is holding foreign keys REFERENCES
 # we will join the tables by checking/equating the id's from these tables
