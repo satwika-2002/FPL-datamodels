5a07334c6a269547b7185a7ba7aa7a5456f0d150
satwika <sathvikachowdary119@gmail.com>
2025-02-21 15:40:38 +0530

    ques-groupby

--- ques7.sql
+++ ques7.sql
@@ -1,9 +1,8 @@
 SELECT 
         p.category,
-        COUNT(p.product_id) AS product_count,
+        COUNT(distinct p.product_id) AS product_count,
         AVG(i.quantity * p.base_price) AS avg_value,
-        MAX(i.quantity) AS max_quantity,
-        p.product_name as highest_stocked_product
+        (select p1.product_name from products p1 join inventory i1 on i1.product_id = p1.product_id where p.category = p1.category order by i1.quantity desc limit 1) as highest_stocked_product
     FROM 
         products p
     JOIN 
@@ -12,5 +11,5 @@ SELECT
         p.category
     having
     	AVG(i.quantity * p.base_price) > 10000
-    	AND product_count >= 3
-    	AND max_quantity > 200;
+    	AND COUNT(DISTINCT p.product_id) >= 3
+    	AND max(i.quantity) > 200;
