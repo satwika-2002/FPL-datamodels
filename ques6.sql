6ea4e3dc5a98eb9265520cb540d3af183eaf50fc
satwika <sathvikachowdary119@gmail.com>
2025-02-24 12:24:46 +0530

    Update ques6.sql

--- ques6.sql
+++ ques6.sql
@@ -12,21 +12,7 @@ CREATE TEMP TABLE inventory_updates (
 (21, 1, 100); -- Insert new
 --Write a MERGE (UPSERT) query to update inventory quantities, inserting new records if they don't exist.
 
---solution query:
-CREATE TEMP TABLE inventory_updates (
-    product_id INT,
-    warehouse_id INT,
-    new_quantity INT
-);
---insert statement that check weather data is already present or not
-insert into inventory_updates(product_id,warehouse_id,new_quantity)
-select 1,1,200
-where not exists(select 1 from inventory_updates where product_id = 1 and warehouse_id = 1);
 
---update statement 
-update inventory_updates 
-set new_quantity = 300
-where exists(select 1 from inventory_updates where product_id = 1 and warehouse_id = 1);
 
 --dynamic query:
 CREATE PROCEDURE UpdateInventoryFromTemp
