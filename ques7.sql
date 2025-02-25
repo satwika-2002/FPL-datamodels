8c47a3447083e777615f5cdb785736b99c756237
satwika <sathvikachowdary119@gmail.com>
2025-02-25 15:48:35 +0530

    Update ques7.sql

--- ques7.sql
+++ ques7.sql
@@ -12,21 +12,7 @@ Electronics | 5            | 12500.75  | Gaming Mouse
 Sports      | 4            | 11200.50  | Yoga Mat
 
 --solution query:
-SELECT 
-        p.category,
-        COUNT(distinct p.product_id) AS product_count,
-        AVG(i.quantity * p.base_price) AS avg_value,
-        (select p1.product_name from products p1 join inventory i1 on i1.product_id = p1.product_id where p.category = p1.category order by i1.quantity desc limit 1) as highest_stocked_product
-    FROM 
-        products p
-    JOIN 
-        inventory i ON p.product_id = i.product_id
-    GROUP BY 
-        p.category
-    having
-    	AVG(i.quantity * p.base_price) > 10000
-    	AND COUNT(DISTINCT p.product_id) >= 3
-    	AND max(i.quantity) > 200;
+SELECT p.category,COUNT(distinct p.product_id) AS product_count,AVG(i.quantity * p.base_price) AS avg_value,(select p1.product_name from products p1 join inventory i1 on i1.product_id = p1.product_id where p.category = p1.category order by i1.quantity desc limit 1) as highest_stocked_product FROM products p JOIN inventory i ON p.product_id = i.product_id GROUP BY p.category having AVG(i.quantity * p.base_price) > 10000 AND COUNT(DISTINCT p.product_id) >= 3 AND max(i.quantity) > 200;
 
 --comments:
 --1. The query selects the category, product count, average value, and highest stocked product name.
